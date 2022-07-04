85ba919579cd6af3d2449c173bb200fbd1735b3b
Ernest Matsevityy <ernest.matsevityy@gmail.com>
2022-07-04 19:30:10 +0300

    adsas

--- main.py
+++ main.py
@@ -1,52 +1,16 @@
-import requests
-import datetime
-from pprint import pprint
-from config import open_weather_token
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def get_weather(city, open_weather_token):
-
-    code_to_smile = {
-        "Clear": "Ясно \U00002600",
-        "Clouds": "Облачно \U00002601",
-        "Rain": "Дождь \U00002614",
-        "Drizzle": "Дождь \U00002614",
-        "Thunderstorm": "Гроза \U000026A1",
-        "Snow": "Снег \U0001F328",
-        "Mist": "Туман \U0001F32B"
-    }
-
-    try:
-        r = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
-        )
-        data = r.json()
-        #pprint(data)
-
-        city = data['name']
-        cur_weater = data["main"]["temp"]
-
-        weather_description = data["weather"][0]["main"]
-        if weather_description in code_to_smile:
-            wd = code_to_smile[weather_description]
-        else:
-            wd = "What the f*ck!"
-
-        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-
-        print(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\nМісто: {city}\nХмарність: {wd}\nТемпература: {cur_weater}C°\nСхід сонця: {sunrise_timestamp}\nЗахід сонця: {sunset_timestamp}")
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-    except Exception as ex:
-        print(ex)
-        print("Перевірте правильність вводу міста")
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-
-def main():
-    city = input("Введіть місто")
-    get_weather(city, open_weather_token)
-
-
-if __name__ == "__main__":
-    main()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
